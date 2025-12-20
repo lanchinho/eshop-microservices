869b66383911c7c73342ff6fb6b1b442899a91f0
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2025-12-20 20:06:40 -0300

    shared project with mediatr abstraction; product feature; (Vertical Slice Architecture) implementation of logic to save a new product  (DB communication to be implemented)

---
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductResult(Guid Id);
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+	: ICommand<CreateProductResult>
+{
+}
+
+internal class CreateProductCommandHandler
+	: ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+	public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+	{
+        var product = new Product
+        {
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+            Name = command.Name,
+            //save to db
+            Id = Guid.NewGuid()
+        };
+
+        return new CreateProductResult(product.Id);
+	}
+}
