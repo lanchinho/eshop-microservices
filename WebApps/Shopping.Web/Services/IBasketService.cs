483a682f564711211f17ee8bbf533b823696a661
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2026-01-03 22:49:44 -0300

    beginning of the BFF app using refit to consume the microservices.

---
+++ WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IBasketService
+{
+    [Get("/basket-service/basket/{userName}")]
+    Task<GetBasketResponse> GetBasket(string userName);
+
+    [Post("/basket-service/basket")]
+    Task<StoreBasketResponse> StoreBasket([Body] StoreBasketRequest request);
+
+    [Delete("/basket-service/basket/{userName}")]
+    Task DeleteBasket(string userName);
+
+    [Post("/basket-service/basket/checkout")]
+    Task<CheckoutBasketResponse> CheckoutBasket([Body] CheckoutBasketRequest request);
+
+}
